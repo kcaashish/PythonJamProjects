75811770510fa999981d1de2a0bf3a1dbb45480d
kcaashish <kcaashish@gmail.com>
2019-09-19 03:00:50 +0545

    Modified a little. Removed unwanted commented codes.

--- src/new.py
+++ src/new.py
@@ -1,9 +1,9 @@
-# it works as volume but get REQUIRED CORRECT ANSWER
+# it gets matching answer as per the sample data in the question
 
 import math
 
 
-def trackFlow(f_in, f_out, r, H, h, t_max, t_open):
+def testFlow(f_in, f_out, r, H, h, t_max, t_open):
     global flow_out
     initialVolume = math.pi * h * r ** 2
     Volumes = list()
@@ -16,8 +16,6 @@ def trackFlow(f_in, f_out, r, H, h, t_max, t_open):
     Times.append(t)
     delta_t = 0.1
     n = 0
-
-    # while (t <= t_max) and (h <= H) and (h >= 0):
 
     if f_in >= f_out:
         while (t <= t_max) and (h <= H) and (h >= 0):
@@ -62,27 +60,11 @@ def trackFlow(f_in, f_out, r, H, h, t_max, t_open):
 
         return [round(x, 2) for x in Volumes], [round(x, 2) for x in Heights], [round(x, 2) for x in Times]
 
-    #     v = round(Volumes[n] + (f_in - flow_out) * delta_t, 10)
-    #     if v > 0:
-    #         Volumes.append(v)
-    #     h = round(Heights[n] + (f_in - flow_out) * delta_t / (math.pi * r ** 2), 10)
-    #     if h >= 0:
-    #         Heights.append(h)
-    #
-    #     n += 1
-    #     Times.append(t)
-    #
-    # return [round(x, 2) for x in Volumes], [round(x, 2) for x in Heights], [round(x, 2) for x in Times]
 
+# volumes, heights, times = testFlow(1, 1, 1, 10, 0, 3, 1)
+volumes, heights, times =  testFlow(1, 5, 1, 10, 0, 5, 2)
+# volumes, heights, times = testFlow(1, 0.5, 1, 10, 0, 3, 1)
 
-# volumes, heights, times = trackFlow(1, 1, 1, 10, 0, 3, 1)
-volumes, heights, times =  trackFlow(1, 5, 1, 10, 0, 5, 2)
-# volumes, heights, times = trackFlow(1, 0.5, 1, 10, 0, 3, 1)
-
-print(len(volumes))
-print(len(heights))
-print(len(times))
-
-print("Volumes ={}".format(volumes))
-print("Heights ={}".format(heights))
-print("Times ={}".format(times))
+print("Volumes = {}".format(volumes))
+print("Heights = {}".format(heights))
+print("Times = {}".format(times))
