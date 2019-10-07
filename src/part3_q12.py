4885644eadaf3b974e4a08853e7e90e623d58d1a
kcaashish <kcaashish@gmail.com>
2019-10-07 22:30:33 +0545

    added plotting to to question 12

--- src/part3_q12.py
+++ src/part3_q12.py
@@ -6,7 +6,12 @@ def angle():
     angles = np.arange(0, 2*np.pi + np.pi, np.pi/3)
     sin = np.sin(angles)
     cos = np.cos(angles)
-    plt.plot(cos, sin)
+
+    plt.plot(cos, sin, marker='.')
+    plt.title("Question 12")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.grid()
     plt.show()
 
 
