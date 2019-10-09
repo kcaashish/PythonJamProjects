2d71a9ce4af75253bd618bb5af5c9f6eca2fd8ec
kcaashish <kcaashish@gmail.com>
2019-10-09 18:03:29 +0545

    added plotting to mcars1

--- src/mcars1.py
+++ src/mcars1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def microcar(expectedList, actualList):
@@ -76,14 +77,62 @@ def microcar(expectedList, actualList):
     return expectedHorDispArray, expectedVertDispArray, actualHorDispArray, actualVertDispArray, expectedDistArray, actualDistArray
 
 
-if __name__ == '__main__':
-    expectedFiles = ["data/expected1.csv", "data/expected2.csv"]
-    actualFiles = ["data/actual1.csv", "data/actual2.csv"]
+def plotmicrocar(expectedList, actualList):
+    expHorDisp, expVerDisp, actHorDisp, actVerDisp, expDist, actDist = microcar(expectedList, actualList)
 
-    a1, a2, a3, a4, a5, a6 = microcar(expectedFiles, actualFiles)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 2, 3)
+    ax3 = fig.add_subplot(2, 2, 4)
+
+    microcars = [1, 2]
+    x_indexes = np.arange(len(microcars))
+    width = 0.25
+
+    ax1.bar(x_indexes - width, expDist, width=width, color="b", label="Expected")
+    ax1.bar(x_indexes, actDist, width=width, color="k", label="Actual")
+    ax1.set_xticks(ticks=x_indexes - width/2)
+    ax1.set_xticklabels(labels=microcars)
+    ax1.set_xlabel("mCar")
+    ax1.set_ylabel("Dist")
+    ax1.set_title("Distance")
+    ax1.legend()
+
+    microcar1 = list()
+    microcar2 = list()
+
+    for mic1, mic2 in zip(expHorDisp, expVerDisp):
+        microcar1.append(mic1)
+        microcar2.append(mic2)
+
+    ax2.scatter(expHorDisp[0], expVerDisp[0], s=100, c='blue', label='Microcar 1')
+    ax2.scatter(expHorDisp[1], expVerDisp[1], s=100, c='orange', label='Microcar 2')
+    ax2.set_xticks(np.arange(-50, 400, 50))
+    ax2.set_yticks(np.arange(-50, 400, 50))
+    ax2.set_title("Expected")
+    ax2.set_xlabel("x Displacement")
+    ax2.set_ylabel("y Disp (m)")
+    ax2.legend()
+
+    ax3.scatter(actHorDisp[0], actVerDisp[0], s=100, c='blue', marker='X', label='Microcar 1')
+    ax3.scatter(actHorDisp[1], actVerDisp[1], s=100, c='orange', marker='X', label='Microcar 2')
+    ax3.set_xticks(np.arange(-50, 400, 50))
+    ax3.set_yticks(np.arange(-50, 400, 50))
+    ax3.set_title("Actual")
+    ax3.set_xlabel("x Displacement")
+    ax3.set_ylabel("y Disp (m)")
+    ax3.legend()
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    a1, a2, a3, a4, a5, a6 = microcar(["data/expected1.csv", "data/expected2.csv"], ["data/actual1.csv", "data/actual2.csv"])
     print(a1)
     print(a2)
     print(a3)
     print(a4)
     print(a5)
     print(a6)
+    plotmicrocar(["data/expected1.csv", "data/expected2.csv"], ["data/actual1.csv", "data/actual2.csv"])
