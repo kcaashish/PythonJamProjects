4f95ae8fc968c39160630441748a3240213d28f6
kcaashish <kcaashish@gmail.com>
2019-10-09 23:57:55 +0545

    added file extension check

--- src/part3_q1.py
+++ src/part3_q1.py
@@ -1,38 +1,40 @@
 def populationgrowth(fileName, n, r, h, t):
-    f = open(fileName, 'w')
     try:
-        assert type(n) == int and n >= 0
+        assert fileName.endswith('.txt')
+        f = open(fileName, 'w')
+
+        try:
+            assert type(n) == int and n >= 0
+        except AssertionError:
+            f.write("Invalid input for initial population.\n")
+
+        try:
+            assert type(r) == float and r > 0
+        except AssertionError:
+            f.write("Invalid input for growth rate.\n")
+
+        try:
+            assert type(h) == float and h > 0
+        except AssertionError:
+            f.write("Invalid input for hours to achieve growth rate.\n")
+
+        try:
+            assert type(t) == float and t > 0
+        except AssertionError:
+            f.write("Invalid input for hours for population growth.\n")
+
+        population = round(n * (r ** (t / h)), 2)
+
+        print("The final population is: " + str(population) + ".\n")
+
+        f.write("The final population is: " + str(population) + ".\n")
+        f.close()
+
+        return population
+
     except AssertionError:
-        f.write("Invalid input for initial population.\n")
-
-    try:
-        assert type(r) == float and r > 0
-    except AssertionError:
-        f.write("Invalid input for growth rate.\n")
-
-    try:
-        assert type(h) == float and h > 0
-    except AssertionError:
-        f.write("Invalid input for hours to achieve growth rate.\n")
-
-    try:
-        assert type(t) == float and t > 0
-    except AssertionError:
-        f.write("Invalid input for hours for population growth.\n")
-
-    if not AssertionError:
-        print("All inputs are valid.")
-        f.write("All the inputs are valid.\n")
-
-    population = round(n * (r ** (t / h)), 2)
-
-    print("The final population is: " + str(population) + ".\n")
-
-    f.write("The final population is: " + str(population) + ".\n")
-    f.close()
-
-    return population
+        print("Invalid file name. Use proper file extension.")
 
 
 if __name__ == '__main__':
-    populationgrowth('organism.txt', 100, 1.4, 6.0, 14.0)
+    populationgrowth('organism', 100, 1.4, 6, 14.0)
