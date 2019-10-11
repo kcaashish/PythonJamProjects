0bc4d18518c727341030bdb5d5633562f7fbe19d
kcaashish <kcaashish@gmail.com>
2019-10-11 22:32:54 +0545

    indentation corrected

--- src/part2_q13.py
+++ src/part2_q13.py
@@ -1,13 +1,12 @@
-
 def stringType(s):
-	if s.isupper():
-		return "Uppercase"
-	elif s.islower():
-		return "Lowercase"
-	elif s.istitle() and " " in s:
-		return "Titlecase"
-	else:
-		return "Capitalised"
+    if s.isupper():
+        return "Uppercase"
+    elif s.islower():
+        return "Lowercase"
+    elif s.istitle() and " " in s:
+        return "Titlecase"
+    else:
+        return "Capitalised"
 
 
 print(stringType("HELLO"))
